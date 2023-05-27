fdbb6fb1fef16e4e23c7e18a20a7bdab3cfb9f16
shsk000 <02luvsic@gmail.com>
2023-05-27 09:18:44 +0900

    feat: add proxy setting

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+const { bootstrap } = require("global-agent");
+process.env.GLOBAL_AGENT_HTTP_PROXY = "http://172.23.240.1:8866";
+bootstrap();
+
+module.exports = nextConfig;
